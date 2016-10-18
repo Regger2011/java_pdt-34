a13fe023d0947b1f0cc90fc397bd04f5f3117756
Regger2011 <t4reg@ya.ru>
2016-10-18 16:13:23 +0400

    повторная загрузка gredle_wrapper

--- sand/src/main/java/ru/stqua/pft/sandbox/prg.java
+++ sand/src/main/java/ru/stqua/pft/sandbox/prg.java
@@ -2,6 +2,13 @@ package ru.stqua.pft.sandbox;
 
 public class prg {
     public static void main(String[] args) {
-        System.out.println("Hi, WRLD");
+        System.out.println("Hi, "+"WRLD");
+        System.out.println();
+        System.out.println("________________________________");
+        System.out.println();
+        int l=6;
+        int s=l*l;
+        System.out.println("Площадь квадрата со стороной равной "+l+" равна "+s );
+
     }
 }
