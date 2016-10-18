1ae2776aa2a8f9174277a3540cb438eafbcfe389
Regger2011 <t4reg@ya.ru>
2016-10-18 14:30:54 +0400

    работа с пакетом
    перемещение в пакет и переименование класса

--- sand/src/main/java/1/prg.java
+++
@@ -1,1 +0,0 @@
-public class prg{public static void main(String[] args){System.out.println("Hi, WRLD");}}
---
+++ sand/src/main/java/ru/stqua/pft/sandbox/prg.java
@@ -0,0 +1,7 @@
+package ru.stqua.pft.sandbox;
+
+public class prg {
+    public static void main(String[] args) {
+        System.out.println("Hi, WRLD");
+    }
+}
