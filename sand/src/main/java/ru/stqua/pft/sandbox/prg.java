7c1a6e75614c7b96b9a7c2e1b307335392afee7f
Regger2011 <t4reg@ya.ru>
2016-10-23 17:54:17 +0300

    Revert "проверено объявление функций и типов переменных"
    This reverts commit a7524521ea08e8a660e69dc1f8e91336b81eb615.

--- sand/src/main/java/ru/stqua/pft/sandbox/prg.java
+++ sand/src/main/java/ru/stqua/pft/sandbox/prg.java
@@ -2,34 +2,13 @@ package ru.stqua.pft.sandbox;
 
 public class prg {
     public static void main(String[] args) {
-        Hi (" This is Me");
-        Hi (" This is U");
-        Hi (" What's UP");
-        double l = 10; /* задаем величину стороны квадрата */
-        double a = 11; /* задаем величину длины прямоугольника */
-        System.out.println("Площадь квадрата со стороной равной " + l + " равна " + Measures(l)); /* вывод результата */
-        double b = 12; /* задаем величину высоты прямоугольника */
-        System.out.println("Площадь прямоугольника со сторонами равными " + a +" и "+b+ " равна " + Measures(a,b)); /* вывод результата */
-    }
-    public static void  Hi (String smbd) {
-            /* String smbd = " - This is Me"; */
-        System.out.println("Hi, "+smbd+"!!!");
+        System.out.println("Hi, "+"WRLD");
         System.out.println();
         System.out.println("________________________________");
         System.out.println();
-    }
-    public static double Measures(double l) {
-        return l*l;
-            /*double s = l * l; /* задаем формулу расчета площади квадрата */
-            /*double p = 4 * l; /* задаем формулу расчета периметра квадрата */
-            /*System.out.println("Площадь квадрата со стороной равной " + l + " равна " + s); /* вывод результата */
-            /*System.out.println("Периметр квадрата со стороной равной " + l + " равен " + p); /* вывод результата */
-    }
-    public static double Measures(double a,double b) {
-        return a*b;
-            /*double s = l * l; /* задаем формулу расчета площади квадрата */
-            /*double p = 4 * l; /* задаем формулу расчета периметра квадрата */
-            /*System.out.println("Площадь квадрата со стороной равной " + l + " равна " + s); /* вывод результата */
-            /*System.out.println("Периметр квадрата со стороной равной " + l + " равен " + p); /* вывод результата */
+        int l=6;
+        int s=l*l;
+        System.out.println("Площадь квадрата со стороной равной "+l+" равна "+s );
+
     }
 }
