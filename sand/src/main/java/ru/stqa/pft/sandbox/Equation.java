299b293353025cd3af60d4cf96e353ab99753826
Regger2011 <t4reg@ya.ru>
2016-12-19 19:33:57 +0300

    if else
    6 tests not optimized

--- sand/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sand/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -6,26 +6,37 @@ public class Equation {
     private double c;
     private int n;
 
-    public Equation (double a, double b, double c) {
+    public Equation(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
 
-        double d=Math.pow(this.b, 2)-4*a*c;
+        double d = Math.pow(this.b, 2) - 4 * a * c;
 
-        if (d>0) {
-            n=2;
-        }
-        else {
-            if (d==0) {
-                n=1;
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    n = -1;
+                } else {
+                    n = 0;
+                }
+            } else {
+                n = 1;
             }
-            else {
-                n=0;
+
+            } else{
+
+                if (d > 0) {
+                    n = 2;
+                } else if (d == 0) {
+                    n = 1;
+                } else {
+                    n = 0;
+                }
             }
-        }
     }
-    public int rootNumber () {
+
+    public int rootNumber() {
         return n;
     }
 }
