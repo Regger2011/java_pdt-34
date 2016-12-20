b72dd781b9a8d690d6461def993b093fef34760b
Regger2011 <t4reg@ya.ru>
2016-12-20 10:52:56 +0300

    пре-Задание 8 в.2
    модификация BaseHelper для проверки на уже существующие поля, а так же
    заполнение дефолтных значений полей в GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Task8/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Task8/tests/GroupCreationTests.java
@@ -10,7 +10,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupsPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("tst1", "tst2", "tst3"));
+        app.getGroupHelper().fillGroupForm(new GroupData("tst1", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
     }
