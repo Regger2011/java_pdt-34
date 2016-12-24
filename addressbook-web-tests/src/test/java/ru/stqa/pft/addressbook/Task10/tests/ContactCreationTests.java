150d1e2f1929f47153d57a86fd2cbb1dda151614
Regger2011 <t4reg@ya.ru>
2016-12-24 18:47:31 +0300

    lambdas in java 8
    для тестов создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Task10/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Task10/tests/ContactCreationTests.java
@@ -19,17 +19,8 @@ public class ContactCreationTests extends TestBase {
         app.getContactHelper().createContact(contact);
         app.getNavigationHelper().gotoHomePage();
         List<ContactData> after=app.getContactHelper().getContactList();
-        //before.add(before.size() + 1);
-        //Assert.assertEquals(after,before);
         Assert.assertEquals(after.size(),before.size()+1);
-
-        int max=0;
-        for (ContactData c : after) {
-            if (c.getId() > max) {
-                max  = c.getId();
-            }
-        }
-        contact.setId(max);
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals (new HashSet<Object>(before),new HashSet<Object>(after));
     }
